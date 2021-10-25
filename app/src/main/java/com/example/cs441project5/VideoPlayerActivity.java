52803dc694c19a17f8617d2d0c93ade51fbad941
jtricozzi <42682429+jtricozzi@users.noreply.github.com>
2021-10-24 22:54:08 -0400

    almost finished the video player activity for displaying the actual video. Controls are almost fully implemented.

--- app/src/main/java/com/example/cs441project5/VideoPlayerActivity.java
+++ app/src/main/java/com/example/cs441project5/VideoPlayerActivity.java
@@ -3,9 +3,13 @@ package com.example.cs441project5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
+    private TextView videoNameTV, videoTimeTV;
+    private ImageButton backIB, forardIB, playPause;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
