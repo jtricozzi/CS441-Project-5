6b8d9a0a74a404c03c64c84b2589deb33829328a
jtricozzi <42682429+jtricozzi@users.noreply.github.com>
2021-10-26 16:39:35 -0400

    Added controls to appear when video is open. Working on hiding those controls currently

--- app/src/main/java/com/example/cs441project5/VideoPlayerActivity.java
+++ app/src/main/java/com/example/cs441project5/VideoPlayerActivity.java
@@ -2,17 +2,123 @@ package com.example.cs441project5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
     private TextView videoNameTV, videoTimeTV;
-    private ImageButton backIB, forardIB, playPause;
+    private ImageButton backIB, forwardIB, playPauseIB;
+    private SeekBar videoSeekbar;
+    private VideoView videoView;
+    private RelativeLayout controlsRL, videoRL;
+    boolean isOpen = true;
+    private String videoName, videoPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+        videoName = getIntent().getStringExtra("videoName");
+        videoPath = getIntent().getStringExtra("videoPath");
+        videoNameTV = findViewById(R.id.idTVVideoTitle);
+        videoTimeTV = findViewById(R.id.idTVTime);
+        backIB = findViewById(R.id.idIBBack);
+        playPauseIB = findViewById(R.id.idIBPlay);
+        forwardIB = findViewById(R.id.idIBForward);
+        videoSeekbar = findViewById(R.id.idSeekBarProgress);
+        videoView = findViewById(R.id.idVideoView);
+        controlsRL = findViewById(R.id.idRLControls);
+        videoRL = findViewById(R.id.idRLVideo);
+
+        videoView.setVideoURI(Uri.parse(videoPath));
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                videoSeekbar.setMax(videoView.getDuration());
+                videoView.start();
+            }
+        });
+
+        videoNameTV.setText(videoName);
+        backIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                videoView.seekTo(videoView.getDuration() - 10000);
+            }
+        });
+
+        forwardIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                videoView.seekTo(videoView.getDuration() + 10000);
+            }
+        });
+
+        playPauseIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(videoView.isPlaying()) {
+                    videoView.pause();
+                    playPauseIB.setImageDrawable(getResources().getDrawable(R.drawable.ic_play));
+                }else {
+                    videoView.start();
+                    playPauseIB.setImageDrawable(getResources().getDrawable(R.drawable.ic_pause);
+                }
+            }
+        });
+
+        videoRL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isOpen) {
+                    hideControls();
+                    isOpen = false;
+                }else {
+                    showControls();
+                    
+                    isOpen = true;
+                }
+            }
+        });
+    }
+
+    private void showControls() {
+        controlsRL.setVisibility(View.VISIBLE);
+
+        final Window window = this.getWindow();
+        if(window == null) {
+            return;
+        }
+        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        View decorView = window.getDecorView();
+        if(decorView != null) {
+            int uiOption = decorView.getSystemUiVisibility();
+            if(Build.VERSION.SDK_INT >= 14){
+                uiOption& = ~View.SYSTEM_UI_FLAG_LOW_PROFILE;
+            }
+
+            if(Build.VERSION.SDK_INT >= 14){
+                uiOption& = ~View.SYSTEM_UI_FLAG_LOW_PROFILE;
+            }
+
+            if(Build.VERSION.SDK_INT >= 14){
+                uiOption& = ~View.SYSTEM_UI_FLAG_LOW_PROFILE;
+            }
+        }
+    }
+
+    private void hideControls() {
     }
 }
